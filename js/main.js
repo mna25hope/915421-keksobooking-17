7c9551ddf423ea0a086ce7dca93d31d0908b65d3
mna25hope <mna25hope@mail.ru>
2019-07-17 05:31:52 +0300

    Личный проект: подробности

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
-"use strict";
+'use strict';
 
 var OBJECT_COUNT = 8;
-var TYPE = ["palace", "flat", "house", "bungalo"];
+var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 
 var PIN_WIDTH = 40;
 var PIN_MIN_Y = 130;
 var PIN_MAX_Y = 630;
 
-var userMap = document.querySelector(".map");
-userMap.classList.remove("map--faded");
+var DEC_RADIX = 10;
 
-var mapPinTemplate = document.querySelector("#pin").content.querySelector(".map__pin");
+var mapElement = document.querySelector('.map');
+var mainPinElement = mapElement.querySelector('.map__pin--main');
 
-var getAvatarImages = function() {
+var formElement = document.querySelector('.ad-form');
+var addressInputElement = formElement.querySelector('input[name=address]');
+
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var getAvatarImages = function () {
   return [
-    "img/avatars/user01.png",
-    "img/avatars/user02.png",
-    "img/avatars/user03.png",
-    "img/avatars/user04.png",
-    "img/avatars/user05.png",
-    "img/avatars/user06.png",
-    "img/avatars/user07.png",
-    "img/avatars/user08.png"
+    'img/avatars/user01.png',
+    'img/avatars/user02.png',
+    'img/avatars/user03.png',
+    'img/avatars/user04.png',
+    'img/avatars/user05.png',
+    'img/avatars/user06.png',
+    'img/avatars/user07.png',
+    'img/avatars/user08.png'
   ];
 };
 
-var getHouseTypes = function() {
-  return ["palace", "flat", "house", "bungalo"];
+var getHouseTypes = function () {
+  return ['palace', 'flat', 'house', 'bungalo'];
 };
 
-//функция случайных чисел
-var getRandomNumber = function(min, max) {
+// функция случайных чисел
+var getRandomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -38,17 +43,17 @@ var getRandomNumber = function(min, max) {
 
 // Возвращает случайную координату X метки
 
-var getRandomPinX = function() {
-  var mapWidth = userMap.clientWidth;
+var getRandomPinX = function () {
+  var mapWidth = mapElement.clientWidth;
   return getRandomNumber(PIN_WIDTH, mapWidth - PIN_WIDTH);
 };
 
 // Возвращает случайную координату Y метки
-var getRandomPinY = function() {
+var getRandomPinY = function () {
   return getRandomNumber(PIN_MIN_Y, PIN_MAX_Y);
 };
 
-var getRandomPins = function(count) {
+var getRandomPins = function (count) {
   var pins = [];
   var avatars = getAvatarImages();
   var houseTypes = getHouseTypes();
@@ -71,13 +76,13 @@ var getRandomPins = function(count) {
 };
 
 // Создает и возвращает элемент метки на основе входных данных
-var createMapPinNode = function(data) {
-  var pinTemplate = document.querySelector("#pin").content.querySelector(".map__pin");
+var createMapPinNode = function (data) {
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var pinElement = pinTemplate.cloneNode(true);
-  var pinElementCover = pinElement.querySelector("img");
+  var pinElementCover = pinElement.querySelector('img');
 
-  pinElement.style.left = data.location.x + "px";
-  pinElement.style.top = data.location.y + "px";
+  pinElement.style.left = data.location.x + 'px';
+  pinElement.style.top = data.location.y + 'px';
   pinElementCover.src = data.author.avatar;
   pinElementCover.alt = data.author.announcementTitle;
 
@@ -85,17 +90,33 @@ var createMapPinNode = function(data) {
 };
 
 // Генерирует и добавляет метки на карту
-var generateAndAppendMapPins = function() {
+var generateAndAppendMapPins = function () {
   var pins = getRandomPins(OBJECT_COUNT);
   var fragment = document.createDocumentFragment();
-  var mapPins = document.querySelector(".map__pins");
+  var mapPins = document.querySelector('.map__pins');
 
-  for (var i = 0; i < pins.length; i++) {
-    var pin = createMapPinNode(pins[i]);
-    fragment.appendChild(pin);
-  }
+  pins.forEach(function (pin) {
+    var pinElement = createMapPinNode(pin);
+    fragment.appendChild(pinElement);
+  });
 
   mapPins.appendChild(fragment);
 };
 
-generateAndAppendMapPins();
+var updateMainPinAddress = function () {
+  var x = parseInt(mainPinElement.style.left, DEC_RADIX);
+  var y = parseInt(mainPinElement.style.top, DEC_RADIX);
+
+  addressInputElement.value = x + ', ' + y;
+};
+
+var mainPinClickHandler = function () {
+  mapElement.classList.remove('map--faded');
+  formElement.classList.remove('ad-form--disabled');
+
+  generateAndAppendMapPins();
+};
+
+mainPinElement.addEventListener('mouseup', mainPinClickHandler);
+
+updateMainPinAddress();
