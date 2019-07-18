9ff2abac47252a9ddc7657d791f463727e8293fc
67e21d <33891482+67e21d@users.noreply.github.com>
2019-07-18 12:13:31 +0900

    12. Личный проект: модуляция

---
+++ js/data.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  var DEC_RADIX = 10;
+
+  var PIN_WIDTH = 40;
+  var PIN_MIN_Y = 130;
+  var PIN_MAX_Y = 630;
+
+  window.data = {
+    DEC_RADIX: DEC_RADIX,
+    PIN_WIDTH: PIN_WIDTH,
+    PIN_MIN_Y: PIN_MIN_Y,
+    PIN_MAX_Y: PIN_MAX_Y,
+    // функция случайных чисел
+    getRandomNumber: function (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  };
+})();
